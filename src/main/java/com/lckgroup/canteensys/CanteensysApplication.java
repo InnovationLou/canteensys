59584e8a84e93426b0f6277bebeeae106a3a0e56
innovation <578777205@qq.com>
2019-12-07 17:49:40 +0800

    build

---
+++ src/main/java/com/lckgroup/canteensys/CanteensysApplication.java
@@ -0,0 +1,13 @@
+package com.lckgroup.canteensys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanteensysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanteensysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lckgroup/canteensys/CanteensysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lckgroup.canteensys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanteensysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
